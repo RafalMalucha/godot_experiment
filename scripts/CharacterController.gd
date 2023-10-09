6a95fe9c993f305c5486d6c8e8c803498f602eaf
Rafal <rafal.z.malucha@gmail.com>
2023-10-09 20:24:16 +0200

    basic movement

--- scripts/CharacterController.gd
+++ scripts/CharacterController.gd
@@ -1,16 +1,26 @@
 extends CharacterBody3D
 
+@onready var head = $Head
 
 @export var current_speed = 10.0
 const JUMP_VELOCITY = 4.5
+var lerp_speed = 10.0
+
+var direction = Vector3.ZERO
 
 const sensitivity = 1
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(event.relative.x * sensitivity * -0.001)
+		head.rotate_x(event.relative.y * sensitivity * -0.001)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _physics_process(delta):
 	
@@ -23,7 +33,7 @@ func _physics_process(delta):
 		current_speed = 10.0
 		
 	if Input.is_action_just_pressed('pause'):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
 		
 	
 	# Add the gravity.
@@ -34,10 +44,11 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*lerp_speed)
 	if direction:
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,4 +1,10 @@
 extends Control
 
+func  _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
